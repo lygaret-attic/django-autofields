842a4624b727fabae77a6ad43faf198acf42ecb7
Jon Raphaelson <lygaret@gmail.com>
2010-02-07 02:27:28 -0700

    added south introspection

--- autofields/fields/__init__.py
+++ autofields/fields/__init__.py
@@ -3,3 +3,33 @@ from automarkdown import *
 from serialized import *
 
 __all__ = [AutoSlugField, AutoMarkdownTextField, SerializedDataField]
+
+
+# custom south stuff
+try:
+	from south.modelsinspector import add_introspection_rules
+	rules = [
+		(
+			(AutoSlugField, ),
+			[],
+			{
+				"prepopulate_from": ["prepopulate_from", {"default": None}],
+				"unique_for_fields": ["unique_for_fields", {"default": []}],
+			}
+		),
+		(
+			(AutoMarkdownTextField, ),
+			[],
+			{
+				"prepopulate_from": ["prepopulate_from", {"default": None}],
+				"only_on_create": ["only_on_create", {"default": False}],
+			}
+		),
+		(
+			(SerializedDataField, ), [], {}
+		),
+	]
+
+	add_introspection_rules(rules, ["^autofields"])
+except ImportError:
+	pass
